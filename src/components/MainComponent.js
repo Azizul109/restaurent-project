ea864159c1b73979c80a4897d5cb2ec241135d59
Azizul109 <mdazizularif109@gmail.com>
2021-12-07 16:05:50 +0600

    second commit main

---
+++ src/components/MainComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
+import Body from '../components/body/Body';
+
+const MainComponent = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Body></Body>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainComponent;
